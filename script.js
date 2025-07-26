62da1984ba0c200af14e1a0903302f3d0f45f220
3mrAlq <omarx4x5@gmail.com>
2025-07-26 17:13:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,788 @@
+const branches = [
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارابن باز",
+  mosqueName: "ملحقة بجامع عبد العزيز ابن باز ",
+  neighborhood: "حي النسيم",
+  lat: 18.288058,
+  lng: 42.752206,
+  mapLink: "https://maps.app.goo.gl/cJcmbebWVMf3Fd2h8?g_st=iw"
+},
+  {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "داراسماء بنت ابي بكر",
+  mosqueName: "ملحقة بجامع الطبري",
+  neighborhood: "حي الراقي",
+  lat: 18.326638,
+  lng: 42.747129,
+  mapLink: "https://maps.app.goo.gl/fpjQieeWCju4qtgP6"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالبتول",
+  mosqueName: "ملحقة بجامع قرى مصلوم ",
+  neighborhood: "حي مصلوم ",
+  lat: 18.329131,
+  lng: 42.707820,
+  mapLink: "https://maps.app.goo.gl/3SGGHwGAjSm4JMr98"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالبسامي",
+  mosqueName: "ملحقة بجامع البسامي",
+  neighborhood: "حي الأسكان",
+  lat: 18.280855,
+  lng: 42.692855,
+  mapLink: "https://maps.app.goo.gl/gNScxaUTvNEAKANq7"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالبينات",
+  mosqueName: "ملحقة بجامع مصعب بن عمير",
+  neighborhood: "حي الربيع",
+  lat: 18.311373,
+  lng: 42.688721,
+  mapLink: "https://maps.app.goo.gl/yAV481xyavHRhQK69"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالحافظات",
+  mosqueName: "ملحقة بمسجد آل دواس",
+  neighborhood: "حي الوسام",
+  lat: 18.349427,
+  lng: 42.762348,
+  mapLink: "https://maps.app.goo.gl/qaae8noUnyR2Gzho9"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالخنساء",
+  mosqueName: "ملحقة بجامع عبد الرحمن البشري",
+  neighborhood: "حي الواحة",
+  lat: 18.332206,
+  lng: 42.694741,
+  mapLink: "https://maps.app.goo.gl/Fz2FPTZDKBhgu2z78"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالدرة",
+  mosqueName: "ملحقة بجامع عثمان بن عفان",
+  neighborhood: "حي الموسى",
+  lat: 18.346866,
+  lng: 42.752152,
+  mapLink: "https://maps.app.goo.gl/zdTPAgF3wdpcWW3N6"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالذاكرات",
+  mosqueName: "ملحقة بمسجد أبو هريرة",
+  neighborhood: "حي الوسام",
+  lat: 18.344111,
+  lng: 42.768359,
+  mapLink: "https://maps.app.goo.gl/xd8h2krWbpLm6R6q7"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالرشد",
+  mosqueName: "دارمستقلة بالقرب من المسجد",
+  neighborhood: "حي السلام خلف المعارض",
+  lat: 18.304628,
+  lng: 42.665890,
+  mapLink: "https://maps.app.goo.gl/89SRZZLoaHoy51PN8"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالريحان",
+  mosqueName: "ملحقة بجامع الريحان",
+  neighborhood: "حي التحلية أو الحضاري",
+  lat: 18.317280,
+  lng: 42.681080,
+  mapLink: "https://maps.app.goo.gl/Lm74h45RecES2scNA"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالزهراوين",
+  mosqueName: "ملحقة بجامع الغروي",
+  neighborhood: "حي حسام",
+  lat: 18.319989,
+  lng: 42.695023,
+  mapLink: "https://maps.app.goo.gl/Jgqa5VSH3eEoS7qd9"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالسنا",
+  mosqueName: "ملحقة بمسجد بلال بن رباح",
+  neighborhood: "حي الموسى ",
+  lat: 18.336318,
+  lng: 42.770409,
+  mapLink: "https://maps.app.goo.gl/MgQromKbBGjPApwa7"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالشيماء",
+  mosqueName: "دور مستقل بجانب جامع قمبر الشرقي ",
+  neighborhood: "حي قمبر",
+  lat: 18.310357,
+  lng: 42.730413,
+  mapLink: "https://maps.app.goo.gl/ufp59XUFH2Snr6hE8"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالصالحات",
+  mosqueName: "ملحقة بجامع أبو بكر الصديق",
+  neighborhood: "حي الرصراص",
+  lat: 18.253409,
+  lng: 42.775892,
+  mapLink: "https://maps.app.goo.gl/WGBPSmad7uGYNo4W7"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دارالوالدين",
+  mosqueName: "دور مستقل ",
+  neighborhood: "حي ذلالة",
+  lat: 18.233121,
+  lng: 42.735783,
+  mapLink: "https://maps.app.goo.gl/FRw7QE86gVpzyXWX7"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار الصفوة",
+  mosqueName: "ملحقة بجامع الصفوة من الجهة الغربية",
+  neighborhood: "حي العزيزية",
+  lat: 18.313892,
+  lng: 42.752322,
+  mapLink: "https://maps.app.goo.gl/4AMaXSbiDqdwcdxd7"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+{
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+},
+    {
+  name: "جمعية نبأ لتحفيظ القرآن الكريم",
+  branchName: "دار",
+  mosqueName: "مسجد",
+  neighborhood: "حي الحيببب",
+  lat: 18888888,
+  lng: 42222222,
+  mapLink: "https"
+}   
+];
+
+
+function getDistance(lat1, lng1, lat2, lng2) {
+  const R = 6371;
+  const dLat = (lat2 - lat1) * Math.PI / 180;
+  const dLng = (lng2 - lng1) * Math.PI / 180;
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+            Math.sin(dLng / 2) * Math.sin(dLng / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return R * c;
+}
+
+function findNearestBranch() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const userLat = position.coords.latitude;
+      const userLng = position.coords.longitude;
+
+      let nearest = null;
+      let minDistance = Infinity;
+
+      branches.forEach(branch => {
+        const dist = getDistance(userLat, userLng, branch.lat, branch.lng);
+        if (dist < minDistance) {
+          minDistance = dist;
+          nearest = branch;
+        }
+      });
+
+      if (nearest) {
+        document.getElementById("result").innerHTML = `
+           أقرب دارلك:<br>
+          <strong>${nearest.branchName}</strong><br>
+          📍 الوصف: ${nearest.mosqueName}<br>
+          🏘️ الحي: ${nearest.neighborhood}<br><br>
+          <a href="${nearest.mapLink}" target="_blank">اضغط لفتح https في خرائط Google</a>
+        `;
+      }
+    }, () => {
+      alert("تعذر تحديد https. تأكد من تفعيل خدمة https.");
+    });
+  } else {
+    alert("المتصفح لا يدعم تحديد https.");
+  }
+}
+
+document.getElementById("findBtn").addEventListener("click", findNearestBranch);  
